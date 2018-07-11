962cf6875c874afbea10ca18a4eb0325945d1bc5
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-07-11 10:54:00 +0100

    Added script for helper functions, nostructure simulations.

---
+++ scripts/functions.R
@@ -0,0 +1,64 @@
+#' Runs PCA on a matrix of log-counts.
+run_PCA = function(log_counts, plot=TRUE, pch=16, ...) {
+    PCA = prcomp(log_counts)
+    var_exp = PCA$sdev^2 
+    if (plot) { 
+        plot(PCA$x[,1], PCA$x[,2], ..., pch=pch,
+	        xlab=sprintf("PC1 (%.1f%%)", var_exp[1]/sum(var_exp) * 100),
+        	ylab=sprintf("PC2 (%.1f%%)", var_exp[2]/sum(var_exp) * 100))
+    }
+    return(invisible(var_exp))
+}
+
+#' Computes log-transformed expression values; returns a transposed matrix.
+lognormalize = function(counts) {
+    lib_size = colSums(counts)
+    lib_size = lib_size/mean(lib_size)
+    log2(t(counts)/lib_size + 1)
+}
+
+#' Identifies and plots the most correlated gene pair.
+plot_correlated = function(log_counts, ...) {
+    COR = cor(log_counts)
+    diag(COR) = 0
+    max_COR = max(COR, na.rm=TRUE)
+    i = which(COR == max_COR, arr.ind=TRUE)[1,]
+    i1 = i[1]
+    i2 = i[2]
+
+    X = log_counts[,i1]
+    Y = log_counts[,i2]
+    plot(X, Y, ...,  
+        xlab=sprintf("Gene %i", i1),  
+        ylab=sprintf("Gene %i", i2))
+
+    fit <- lm(Y ~ X)
+    abline(a=coef(fit)[1], b=coef(fit)[2], col="red")
+    legend("bottomright", bty="n", legend=substitute(R^2~"="~r2, 
+        list(r2=round(max_COR^2, 2))), text.col="red")
+}
+
+#' Creates a heatmap with mean-centering and limits.
+create_heatmap = function(exprs, cluster, is_de, limit=0.5, cluster_cols, de_cols) {
+    uniq_clusters = unique(cluster)
+    reorder = order(rowMeans(exprs[,cluster==uniq_clusters[1]]) - rowMeans(exprs[,cluster==uniq_clusters[2]]))
+    exprs = exprs[reorder,]
+    is_de = is_de[reorder]
+
+    exprs = exprs - rowMeans(exprs)
+    exprs[exprs > limit] = limit
+    exprs[exprs < -limit] = -limit
+
+    colnames(exprs) = seq_len(ncol(exprs))
+    rownames(exprs) = seq_len(nrow(exprs))
+
+    require(pheatmap)
+    pheatmap(exprs, cluster_col=FALSE, cluster_row=FALSE,
+        annotation_col=data.frame(Cluster=as.character(cluster), row.names=colnames(exprs)),
+        annotation_row=data.frame(DE=as.character(is_de), row.names=rownames(exprs)),
+        annotation_colors=list(Cluster=cluster_cols, DE=de_cols),
+        show_rownames=FALSE, show_colnames=FALSE)
+}
+
+#' Also creating a directory.
+dir.create("pics", showWarnings=FALSE)
---
+++ scripts/nostruct_sim.R
@@ -0,0 +1,40 @@
+source("functions.R")
+
+# Generating Poisson-distributed counts.
+set.seed(1000)
+ngenes = 1000
+ncells = 500
+lambda = runif(ngenes, 0, 10)
+counts = matrix(rpois(ncells * ngenes, lambda=lambda), ncol=ncells)
+
+# Creating a PCA plot on the log-normalized counts from MAGIC.
+library(Rmagic) 
+MGC = run_magic(t(counts), t_diffusion=10)
+
+pdf("pics/nostructure_with_magic.pdf")
+lmgc = lognormalize(t(MGC))
+plot_limits = c(-0.005, 0.005)
+run_PCA(lmgc, xlim=plot_limits, ylim=plot_limits)
+plot_correlated(lmgc)
+dev.off()
+
+# Creating a PCA plot on the reference.
+pdf("pics/nostructure_without_magic.pdf")
+lref = lognormalize(counts)
+run_PCA(lref)
+plot_correlated(lref)
+dev.off()
+
+# Varying the diffusion time.
+output = numeric(20)
+for (i in seq_along(output)) {
+    MGC = run_magic(t(counts), t_diffusion=i)
+    lmgc = log2(MGC/rowSums(MGC) + 1)
+    var_exp = run_PCA(lmgc, plot=FALSE)
+    output[i] = var_exp[1]/sum(var_exp) * 100
+}
+
+pdf("pics/nostructure_time.pdf", width=10, height=5)
+plot(output, xlab="Diffusion time", ylab="Variance explained by PC1 (%)", pch=16)
+lines(output)
+dev.off()
