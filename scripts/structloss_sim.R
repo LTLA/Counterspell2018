9cf33f43b144fd6ba2e5fb88ffc4ef4ca5817ed4
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-07-12 10:35:13 +0100

    Updated structure loss script for new MAGIC call.

--- scripts/structloss_sim.R
+++ scripts/structloss_sim.R
@@ -8,10 +8,10 @@ ncells = 500
 lambda = runif(ngenes, 0, 10)
 counts = rbind(
     matrix(rpois(ncells * ngenes, lambda=lambda), ncol=ncells),
-    matrix(rpois(ncells * 50, lambda=rep(c(0, 5), ncells/2)), ncol=ncells, byrow=TRUE),
-    matrix(rpois(ncells * 50, lambda=rep(c(5, 0), ncells/2)), ncol=ncells, byrow=TRUE),
-    matrix(rpois(ncells * 50, lambda=rep(c(0, 2), each=ncells/2)), ncol=ncells, byrow=TRUE),
-    matrix(rpois(ncells * 50, lambda=rep(c(2, 0), each=ncells/2)), ncol=ncells, byrow=TRUE)
+    matrix(rpois(ncells * 50, lambda=rep(c(1, 5), ncells/2)), ncol=ncells, byrow=TRUE),
+    matrix(rpois(ncells * 50, lambda=rep(c(5, 1), ncells/2)), ncol=ncells, byrow=TRUE),
+    matrix(rpois(ncells * 50, lambda=rep(c(1, 2), each=ncells/2)), ncol=ncells, byrow=TRUE),
+    matrix(rpois(ncells * 50, lambda=rep(c(2, 1), each=ncells/2)), ncol=ncells, byrow=TRUE)
 )
 
 primary_sep = rep(c(TRUE, FALSE), ncells/2)
@@ -21,18 +21,17 @@ color = ifelse(secondary_sep, "forestgreen", "goldenrod")
 de_cols = rep(c("grey60", "dodgerblue"), c(ngenes, nrow(counts) - ngenes))
 
 # Creating a PCA plot on the log-normalized counts from MAGIC.
-library(Rmagic) 
-MGC = run_magic(t(counts), t_diffusion=10)
+library(Rmagic)
+MGC = run_MAGIC(counts, t=10)
 
 pdf("pics/minor_with_magic.pdf")
-lmgc = lognormalize(t(MGC))
-run_PCA(lmgc, same_xy=TRUE, col=color, pch=pch)
+run_PCA(MGC, same_xy=TRUE, col=color, pch=pch)
 
-plot(compute_logFC(lmgc, primary_sep),
-    compute_logFC(lmgc, secondary_sep),
+plot(compute_logFC(MGC, primary_sep),
+    compute_logFC(MGC, secondary_sep),
     xlab=expression(Log[2]~"fold change (primary)"),
     ylab=expression(Log[2]~"fold change (secondary)"),
-    xlim=c(-3, 3), ylim=c(-2, 2),
+    xlim=c(-2, 2), ylim=c(-2, 2),
     col=de_cols, pch=16)
 dev.off()
 
@@ -45,6 +44,6 @@ plot(compute_logFC(lref, primary_sep),
     compute_logFC(lref, secondary_sep),
     xlab=expression(Log[2]~"fold change (primary)"),
     ylab=expression(Log[2]~"fold change (secondary)"),
-    xlim=c(-3, 3), ylim=c(-2, 2),
+    xlim=c(-2, 2), ylim=c(-2, 2),
     col=de_cols, pch=16)
 dev.off()
