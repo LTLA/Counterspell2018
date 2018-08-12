f561dabb6dc229c54accd2a3b5d1e49a415f0024
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-08-12 19:13:15 +0100

    Tidied up simulation code formatting, output.

--- scripts/nostruct_sim.R
+++ scripts/nostruct_sim.R
@@ -2,26 +2,24 @@ source("functions.R")
 
 # Generating Poisson-distributed counts.
 set.seed(200)
-ngenes = 1000
-ncells = 500
-lambda = runif(ngenes, 0, 10)
-size_fac = runif(ncells, 1, 2)
-counts = matrix(rpois(ncells * ngenes, lambda=outer(lambda, size_fac)), ncol=ncells)
+ngenes <- 1000
+ncells <- 500
+lambda <- runif(ngenes, 0, 10)
+size_fac <- runif(ncells, 1, 2)
+counts <- matrix(rpois(ncells * ngenes, lambda=outer(lambda, size_fac)), ncol=ncells)
 
 library(viridis)
-col = viridis(100)[cut(size_fac, 100)]
+col <- viridis(100)[cut(size_fac, 100)]
+
+MGC <- run_MAGIC(counts, t=10)
+lref <- lognormalize(counts)
 
 # Creating a PCA plot on the log-normalized counts from MAGIC.
-MGC = run_MAGIC(counts, t=10)
-
-pdf("pics/nostructure_with_magic.pdf")
+pdf("pics/nostructure.pdf")
+par(cex.lab=1.4)
 run_PCA(MGC, same_xy=TRUE, col=col)
 plot_correlated(MGC)
-dev.off()
 
-# Creating a PCA plot on the reference.
-pdf("pics/nostructure_without_magic.pdf")
-lref = lognormalize(counts)
 run_PCA(lref, same_xy=TRUE, col=col)
 plot_correlated(lref, position="topleft")
 dev.off()
--- scripts/structloss_sim.R
+++ scripts/structloss_sim.R
@@ -20,29 +20,29 @@ secondary_sep = rep(c(TRUE, FALSE), each=ncells/2)
 color = ifelse(secondary_sep, "forestgreen", "goldenrod")
 de_cols = rep(c("grey60", "dodgerblue"), c(ngenes, nrow(counts) - ngenes))
 
-# Creating a PCA plot on the log-normalized counts from MAGIC.
+# Running MAGIC.
 MGC = run_MAGIC(counts, t=10)
+lref = lognormalize(counts)
 
-pdf("pics/minor_with_magic.pdf")
+# Creating a PCA plot on the log-normalized counts from MAGIC.
+pdf("pics/structloss.pdf")
+par(cex.lab=1.4)
 run_PCA(MGC, same_xy=TRUE, col=color, pch=pch)
 
 plot(compute_logFC(MGC, primary_sep),
     compute_logFC(MGC, secondary_sep),
-    xlab="Difference (primary)",
-    ylab="Difference (secondary)",
+    xlab="Difference in means (primary)",
+    ylab="Difference in means (secondary)",
     xlim=c(-2, 2), ylim=c(-2, 2),
     col=de_cols, pch=16)
-dev.off()
 
 # Creating a PCA plot on the reference.
-pdf("pics/minor_without_magic.pdf")
-lref = lognormalize(counts)
 run_PCA(lref, same_xy=TRUE, col=color, pch=pch)
 
 plot(compute_logFC(lref, primary_sep),
     compute_logFC(lref, secondary_sep),
-    xlab=expression(Log[2]~"fold change (primary)"),
-    ylab=expression(Log[2]~"fold change (secondary)"),
+    xlab="Difference in means (primary)",
+    ylab="Difference in means (secondary)",
     xlim=c(-2, 2), ylim=c(-2, 2),
     col=de_cols, pch=16)
 dev.off()
