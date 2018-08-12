6fd95ffabe90fcea51ea8397b78a452088eae84e
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-08-12 19:13:35 +0100

    Report Wilcoxon p-values for fake-de simulation.

--- scripts/fakede_sim.R
+++ scripts/fakede_sim.R
@@ -3,29 +3,38 @@ source("functions.R")
 # Scenario 2: DE between two clusters
 
 set.seed(400)
-ngenes = 900
-ncells = 500
-lambda = runif(ngenes, 0, 10)
-counts = rbind(
+ngenes <- 9900
+ncells <- 500
+lambda <- runif(ngenes, 0, 10)
+counts <- rbind(
     matrix(rpois(ncells * ngenes, lambda=lambda), ncol=ncells),
     matrix(rpois(ncells * 50, lambda=rep(c(0, 5), each=ncells/2)), ncol=ncells, byrow=TRUE),
     matrix(rpois(ncells * 50, lambda=rep(c(5, 0), each=ncells/2)), ncol=ncells, byrow=TRUE)
 )
 
-cluster = rep(1:2, each=ncells/2)
-cluster_cols = c(`1`="forestgreen", `2`="goldenrod")
-is_de = c(logical(ngenes), !logical(nrow(counts)-ngenes))
-de_cols = c(`TRUE`="dodgerblue", `FALSE`="grey60")    
+cluster <- rep(1:2, each=ncells/2)
+cluster_cols <- c(`1`="forestgreen", `2`="goldenrod")
+is_de <- c(logical(ngenes), !logical(nrow(counts)-ngenes))
+de_cols <- c(`TRUE`="dodgerblue", `FALSE`="grey60")    
 
-# Creating a PCA plot on the log-normalized counts from MAGIC.
-MGC = run_MAGIC(counts, t=10)
+# Running MAGIC.
+MGC <- run_MAGIC(counts, t=10)
+lref <- lognormalize(counts)
 
-png("pics/clusters_with_magic.png", units="in", width=7, height=7, res=150, pointsize=12)
-create_heatmap(t(MGC), cluster=cluster, is_de=is_de, limit=0.2, cluster_cols=cluster_cols, de_cols=de_cols)
-dev.off()
+# Applying a series of Wilcoxon rank-sum tests.
 
-# Creating a PCA plot on the reference.
-png("pics/clusters_without_magic.png", units="in", width=7, height=7, res=150, pointsize=12)
-lref = lognormalize(counts)
-create_heatmap(t(lref), cluster=cluster, is_de=is_de, limit=0.2, cluster_cols=cluster_cols, de_cols=de_cols)
+p.mgc <- numeric(nrow(counts))
+for (i in seq_along(p.mgc)) {
+    p.mgc[i] <- wilcox.test(MGC[cluster==1,i][,1], MGC[cluster==2,i][,1])$p.value
+}
+
+p.log <- numeric(nrow(counts))
+for (i in seq_along(p.log)) {
+    p.log[i] <- wilcox.test(lref[cluster==1,i], lref[cluster==2,i])$p.value
+}
+
+pdf("pics/fakede.pdf")
+breaks <- seq(0, 1, length.out=50)
+hist(p.mgc[!is_de], breaks=breaks, col="violet")
+hist(p.log[!is_de], breaks=breaks, col="lavender")
 dev.off()
